d71575ebdc26815c8349cd69b543e51d6679f04f
Senthil Sivaramakrishnan <senthilsivaramakrishnan@Senthils-MacBook-Pro.local>
2020-02-26 17:05:50 +0100

    REMOVE unused code

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,29 +1,3 @@
-/* import axios from 'axios';
-import API from '../../api/config';
-
-export const fetchFlightDetails = (result: Array<any>) => ({
-    type: FETCH_FLIGHT_DETAILS,
-    result
-})
-
-export const receivedFlightDetails = (json: any) => ({
-    type: RECEIVE_FLIGHT_DETAILS,
-    json: json.data
-})
-
-const getFlights = (url: string) => axios.get(url);
-
-export const fetchAllData = () => {
-    return (dispatch: any) => {
-      return getFlights(API.cheap_flights)
-        .then(response => {
-          dispatch(fetchFlightDetails(response.data))
-        })
-        .catch(error => {
-          throw(error);
-        });
-    };
-  }; */
 
   export * from './flightInfoActions'
   export * from './flightInfoActions.types'
